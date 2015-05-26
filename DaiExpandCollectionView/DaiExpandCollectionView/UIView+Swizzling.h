d3af63720b39473fd2a33d7288c8600288198c83
DaidoujiChen <daidoujichen@gmail.com>
2015-05-26 11:29:17 +0800

    fix #2, 避開超出範圍的 cell 還需要 rendered 的問題

---
+++ DaiExpandCollectionView/DaiExpandCollectionView/UIView+Swizzling.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Swizzling.h
+//  DaiExpandCollectionView
+//
+//  Created by DaidoujiChen on 2015/5/26.
+//  Copyright (c) 2015年 DaidoujiChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Swizzling)
+
+@end
---
+++ DaiExpandCollectionView/DaiExpandCollectionView/UIView+Swizzling.m
@@ -0,0 +1,58 @@
+//
+//  UIView+Swizzling.m
+//  DaiExpandCollectionView
+//
+//  Created by DaidoujiChen on 2015/5/26.
+//  Copyright (c) 2015年 DaidoujiChen. All rights reserved.
+//
+
+#import "UIView+Swizzling.h"
+#import <objc/runtime.h>
+#import "DaiExpandCollectionView.h"
+
+@implementation UIView (Swizzling)
+
+#pragma mark - life cycle
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        [self swizzling:@selector(snapshotViewAfterScreenUpdates:) to:@selector(swizzling_snapshotViewAfterScreenUpdates:)];
+    });
+}
+
+#pragma mark - swizzling method
+
+- (UIView *)swizzling_snapshotViewAfterScreenUpdates:(BOOL)afterUpdates {
+    if ([self.superview isKindOfClass:[DaiExpandCollectionView class]]) {
+        DaiExpandCollectionView *collectionView = (DaiExpandCollectionView *)self.superview;
+        CGRect visibleFrame = CGRectMake(collectionView.contentOffset.x, collectionView.contentOffset.y, collectionView.contentOffset.x + collectionView.bounds.size.width, collectionView.contentOffset.y + collectionView.bounds.size.height);
+        if (!CGRectContainsRect(visibleFrame, self.frame)) {
+            return nil;
+        }
+    }
+    return [self swizzling_snapshotViewAfterScreenUpdates:afterUpdates];
+}
+
+#pragma mark - private instance method
+
++ (void)swizzling:(SEL)before to:(SEL)after {
+    Class class = [self class];
+    
+    SEL originalSelector = before;
+    SEL swizzledSelector = after;
+    
+    Method originalMethod = class_getInstanceMethod(class, originalSelector);
+    Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+    
+    BOOL didAddMethod = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+    
+    if (didAddMethod) {
+        class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+    }
+    else {
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+    }
+}
+
+@end
